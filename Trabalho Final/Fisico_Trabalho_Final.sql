6d75eb68fec42d51344f387d2d87b2af447d5f98
Kaique Abranches de Paula <kaiquep.abranches@gmail.com>
2026-03-27 17:56:00 -0300

    Trabalho final BD - segunda parte

--- Trabalho Final/Fisico_Trabalho_Final.sql
+++ Trabalho Final/Fisico_Trabalho_Final.sql
@@ -60,6 +60,12 @@ create type tipo_laboratorio as enum(
 'externo'
 );
 
+create type tipo_especialidade as enum(
+	'cardiologista',
+	'pediatria',
+	'clinico geral'
+);
+
 -- TODAS AS TABELAS
 
 create table hospital (
@@ -122,9 +128,113 @@ foreign key(fk_id_plano_de_saude) references plano_de_saude(id_plano_de_saude)
 on delete cascade on update no action
 );
 
+create table endereco(
+	id_endereco serial primary key,
+	cep int unique not null,
+	rua varchar(100) not null,
+	bairro varchar(100) not null,
+	cidade varchar(100) not null,
+	estado varchar(100) not null,
+	numero_casa int not null
+);
+
+create table paciente(
+	id_paciente serial primary key,
+	nome varchar(60),
+	cpf char(11) unique,
+	fk_endereco int default null,
+	fk_id_telefone int default null,
+	data_nascimento date,
+	foreign key(fk_id_telefone) references telefone(id_telefone)
+	on delete no action
+	on update no action,
+	foreign key(fk_endereco) references endereco(id_endereco)
+);
+
+-- CÓDIGO ANTERIOR
+/*
+	create table internacao(
+		id_internacao serial primary key,
+		data_entrada date,
+		data_saida date check(data_saida > data_entrada),
+		status boolean generated always as
+						( case 
+					when current_date > data_saida then true
+					else false
+		   			end
+		   				) stored,
+			fk_id_paciente int,
+			fk_id_leito int,
+			foreign key(fk_id_paciente) references paciente(id_paciente)
+			on delete set null
+			on update no action,
+			foreign key(fk_id_leito) references leito(id_leito)
+			on delete set null
+			on update no action
+);*/
 
 
+create table internacao(
+		id_internacao serial primary key,
+		data_entrada date,
+		data_saida date check(data_saida > data_entrada),
+		status boolean,
+		fk_id_paciente int,
+		fk_id_leito int,
+		foreign key(fk_id_paciente) references paciente(id_paciente)
+		on delete set null
+		on update no action,
+		foreign key(fk_id_leito) references leito(id_leito)
+		on delete set null
+		on update no action
+);
 
+create table medico(
+	id_medico serial primary key,
+	crm char(8) unique not null,
+	nome varchar(60) not null,
+	especialidade tipo_especialidade not null,
+	fk_telefone int,
+	foreign key(fk_telefone) references telefone(id_telefone)
+);
+
+create table pesquisa_satisfacao(
+	id_pesquisa serial primary key,
+	data_resposta date,
+	nota_geral int check(nota_geral between 1 and 5),
+	comentario text,
+	recomendaria boolean,
+	tempo_espera_avaliacao int check(tempo_espera_avaliacao between 1 and 5)
+);
+
+create table atendimento(
+	id_atendimento serial primary key,
+	data_atendimento timestamp,
+	tipo tipo_atendimento,
+	observacoes text,
+	status status_atendimento,
+	fk_id_medico int,
+	fk_id_paciente int,
+	fk_id_pesquisa int,
+	foreign key(fk_id_medico) references medico(id_medico)
+	on delete cascade
+	on update no action,
+	foreign key(fk_id_paciente) references paciente(id_paciente)
+	on delete cascade
+	on update no action,
+	foreign key (fk_id_pesquisa) references pesquisa_satisfacao(id_pesquisa)
+	on delete cascade
+	on update no action
+);
+
+create table prescricao(
+	id_prescricao serial primary key,
+	data_prescricao date,
+	fk_id_atendimento int,
+	foreign key(fk_id_atendimento) references atendimento(id_atendimento)
+	on update no action
+	on delete cascade
+);
 
 
 
